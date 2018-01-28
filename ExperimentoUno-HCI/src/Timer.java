61e888610633551792f3c5890dd76b8d06a9ed60
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2018-01-28 17:12:18 -0500

    hola 3

--- ExperimentoUno-HCI/src/Timer.java
+++ ExperimentoUno-HCI/src/Timer.java
@@ -1,9 +1,14 @@
+import java.util.concurrent.TimeUnit;
 
 public class Timer extends Thread {
 
 	public Timer() {
 		long startTime = System.nanoTime();
-		Thread.sleep(1000 * 10);
+		try {
+			Thread.sleep(1000 * 10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		long difference = System.nanoTime() - startTime;
 		System.out.println("Total execution time: " + String.format("%d min, %d sec",
 				TimeUnit.NANOSECONDS.toHours(difference), TimeUnit.NANOSECONDS.toSeconds(difference)
